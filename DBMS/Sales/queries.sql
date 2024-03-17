653a1d48ebd5f0ce02db9d82b5c64c449d25240d
RAJJIT <2.0lairaj@gmail.com>
2024-03-17 10:15:48 +0530

    added-queries-4-9

--- DBMS/Sales/queries.sql
+++ DBMS/Sales/queries.sql
@@ -53,3 +53,72 @@ select * from Customer where RATING > 100 or(CITY="LONDON" and RATING <= 100);
 -- | 2008 | Chirag  | London |    100 | 1001 |
 -- | 2006 | Chinmay | Surat  |    400 | 1007 |
 -- +------+---------+--------+--------+------+
+
+-- 4. List of all orders for more than Rs 1000 except the orders of SNUM < 1006 of 10/03/21
+select * from Orders where AMOUNT > 1000 and (SNUM >= 1006 or ODATE != '2021-03-10');
+-- Output
+-- +------+---------+------------+------+------+
+-- | ONUM | AMOUNT  | ODATE      | CNUM | SNUM |
+-- +------+---------+------------+------+------+
+-- | 3002 | 1800.21 | 2021-03-01 | 2019 | 1004 |
+-- | 3005 | 1650.54 | 2021-03-01 | 2018 | 1002 |
+-- | 3006 | 1808.61 | 2021-03-01 | 2016 | 1005 |
+-- | 3009 | 1719.32 | 2021-04-10 | 2013 | 1008 |
+-- | 3008 | 7433.00 | 2021-04-11 | 2020 | 1005 |
+-- | 3010 | 3108.59 | 2021-04-11 | 2012 | 1002 |
+-- | 3011 | 8981.88 | 2021-04-11 | 2011 | 1008 |
+-- +------+---------+------------+------+------+
+
+-- 5. Produce the order no, amount and date of all orders
+select ONUM, AMOUNT, ODATE from Orders;
+-- Output
+-- +------+---------+------------+
+-- | ONUM | AMOUNT  | ODATE      |
+-- +------+---------+------------+
+-- | 3001 |   17.78 | 2021-03-01 |
+-- | 3003 |  676.91 | 2021-03-01 |
+-- | 3002 | 1800.21 | 2021-03-01 |
+-- | 3005 | 1650.54 | 2021-03-01 |
+-- | 3006 | 1808.61 | 2021-03-01 |
+-- | 3009 | 1719.32 | 2021-04-10 |
+-- | 3007 |   57.57 | 2021-04-10 |
+-- | 3008 | 7433.00 | 2021-04-11 |
+-- | 3010 | 3108.59 | 2021-04-11 |
+-- | 3011 | 8981.88 | 2021-04-11 |
+-- +------+---------+------------+
+
+-- 6. GIve all the information about all the customer with salesman 1001
+select * from Customer where SNUM = 1001;
+-- Output
+-- +------+--------+--------+--------+------+
+-- | CNUM | CNAME  | CITY   | RATING | SNUM |
+-- +------+--------+--------+--------+------+
+-- | 2001 | Harsh  | London |    100 | 1001 |
+-- | 2008 | Chirag | London |    100 | 1001 |
+-- +------+--------+--------+--------+------+
+
+-- 7. List all the customers whose name starts with A
+select * from Customer where CNAME like 'A%';
+-- Output (the output will be empty because there is no customer whose name starts with A)
+-- Empty set (0.00 sec)
+
+-- 8. List all the customers whose name ends with H
+select * from Customer where CNAME like '%H';
+-- Output
+-- +------+-------+--------+--------+------+
+-- | CNUM | CNAME | CITY   | RATING | SNUM |
+-- +------+-------+--------+--------+------+
+-- | 2001 | Harsh | London |    100 | 1001 |
+-- +------+-------+--------+--------+------+
+
+-- 9. List all the customers whose name begins with letter A to G
+select * from Customer where left(CNAME, 1) between 'A' and 'G';
+-- Output
+-- +------+---------+--------+--------+------+
+-- | CNUM | CNAME   | CITY   | RATING | SNUM |
+-- +------+---------+--------+--------+------+
+-- | 2002 | Gita    | Rome   |    200 | 1003 |
+-- | 2004 | Govind  | Bombay |    300 | 1002 |
+-- | 2008 | Chirag  | London |    100 | 1001 |
+-- | 2006 | Chinmay | Surat  |    400 | 1007 |
+-- +------+---------+--------+--------+------+
