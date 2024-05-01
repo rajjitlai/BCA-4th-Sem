ee0ef7120bbae1524ec458fc186ceccfb2039a7b
RAJJIT LAISHRAM <2.0lairaj@gmail.com>
2024-05-01 16:31:13 +0530

    fixes-11.2

--- DBMS/Practical/Practical 4.sql
+++ DBMS/Practical/Practical 4.sql
@@ -1,4 +1,28 @@
 Date: 18/04/2024
+-- Q. Display the list of Orders where SNUM is either 1004, 1008, 1007.
+select * from Orders where SNUM in (1004,1008,1007);
+
+-- Output
+-- +------+---------+------------+------+------+
+-- | ONUM | AMOUNT  | ODATE      | CNUM | SNUM |
+-- +------+---------+------------+------+------+
+-- | 3001 | 17.78   | 2021-03-01 | 2009 | 1007 |
+-- | 3002 | 1800.21 | 2021-03-01 | 2019 | 1004 |
+-- | 3007 | 57.57   | 2021-04-10 | 2017 | 1004 |
+-- | 3009 | 1719.32 | 2021-04-10 | 2013 | 1008 |
+-- | 3011 | 8981.88 | 2021-04-11 | 2011 | 1008 |
+-- +------+---------+------------+------+------+
+
+-- Q. Display the list of Customer whose name is ending with letter "D"
+select * from Customer where CNAME like "%d";
+
+-- Output 
+-- +------+--------+--------+--------+------+
+-- | CNUM | CNAME  | CITY   | RATING | SNUM |
+-- +------+--------+--------+--------+------+
+-- | 2004 | Govind | Bombay | 300    | 1002 |
+-- +------+--------+--------+--------+------+
+
 -- Queries before performing any sql commands
 alter table Salesman modify COLUMN COMMISSION varchar(20);
 
@@ -18,31 +42,7 @@ select * from Salesman;
 -- | 1006 | SEJAL   | SPAIN     | 12%        |
 -- +------+---------+-----------+------------+
 
--- Q.2 Display the list of Orders where SNUM is either 1004, 1008, 1007.
-select * from Orders where SNUM in (1004,1008,1007);
-
--- Output
--- +------+---------+------------+------+------+
--- | ONUM | AMOUNT  | ODATE      | CNUM | SNUM |
--- +------+---------+------------+------+------+
--- | 3001 | 17.78   | 2021-03-01 | 2009 | 1007 |
--- | 3002 | 1800.21 | 2021-03-01 | 2019 | 1004 |
--- | 3007 | 57.57   | 2021-04-10 | 2017 | 1004 |
--- | 3009 | 1719.32 | 2021-04-10 | 2013 | 1008 |
--- | 3011 | 8981.88 | 2021-04-11 | 2011 | 1008 |
--- +------+---------+------------+------+------+
-
--- Q.3 Display the list of Customer whose name sending with letter "D"
-select * from Customer where CNAME like "%d";
-
--- Output 
--- +------+--------+--------+--------+------+
--- | CNUM | CNAME  | CITY   | RATING | SNUM |
--- +------+--------+--------+--------+------+
--- | 2004 | Govind | Bombay | 300    | 1002 |
--- +------+--------+--------+--------+------+
-
--- Q.4 Display the total SNUM, Average, SNUM, MAX SNUM, MIN SNUM of Orders Table
+-- Q.2 Display the total SNUM, Average, SNUM, MAX SNUM, MIN SNUM of Orders Table
 select sum(SNUM), avg(SNUM), max(SNUM), min(SNUM) from Orders;
 
 -- Output
@@ -52,7 +52,7 @@ select sum(SNUM), avg(SNUM), max(SNUM), min(SNUM) from Orders;
 -- | 10051     | 1005.1000 | 1008      | 1002      |
 -- +-----------+-----------+-----------+-----------+
 
--- Q.5 Display the name of Salesman whose Customer rating is 100.
+-- Q.3 Display the name of Salesman whose Customer rating is 100.
 select SNAME from Salesman where SNUM in (select SNUM from Customer where RATING = 100);
 
 -- Output
@@ -62,7 +62,7 @@ select SNAME from Salesman where SNUM in (select SNUM from Customer where RATING
 -- | RAM   |
 -- +-------+
 
--- Q.6 Display the customer number of customer whose commission is > 11
+-- Q.4 Display the customer number of customer whose commission is > 11
 select CNAME from Customer where SNUM in (select SNUM from Salesman where COMMISSION > 11);
 
 -- Output
