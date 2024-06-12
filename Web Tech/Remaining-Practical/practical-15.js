f3ebdcd11147268da34102990b3721963977f5db
RAJJIT <2.0lairaj@gmail.com>
2024-06-12 21:58:47 +0530

    practical-15-added

---
+++ Web Tech/Remaining-Practical/practical-15.js
@@ -0,0 +1,14 @@
+// a program to sum and multiply of two numbers using JavaScript using functions
+
+function sum(a, b) {
+    return a + b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+var sum = sum(10, 20)
+var mul = multiply(33, 10)
+console.log(sum)
+console.log(mul)
